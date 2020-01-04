621c9e0991c6164bea6669ba207f57033e10cfac
Leo Zhang <leo@leozhang.me>
2020-01-04 13:27:25 -0800

    wip: Scaffolding for endpoints

--- cmd/pooterserver/main.go
+++ cmd/pooterserver/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 func main() {
-	fmt.Println("hello world")
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Post("/users", func(w http.ResponseWriter, r *http.Request) {})
+
+	r.Post("/users/{userID}/followers", func(w http.ResponseWriter, r *http.Request) {})
+
+	r.Post("/posts", func(w http.ResponseWriter, r *http.Request) {})
+
+	r.Get("/users/{userID}/feed", func(w http.ResponseWriter, r *http.Request) {})
+
+	r.Get("/users/{userID}/posts", func(w http.ResponseWriter, r *http.Request) {})
+
+	log.Println("Listening on port :8000")
+	http.ListenAndServe(":8000", r)
 }
