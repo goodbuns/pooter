10d2d7b143394f6224df992d058dbfbb2f5fdc73
Leo Zhang <leo@leozhang.me>
2020-01-05 02:03:21 -0800

    wip: Sample Twitter firehose for live tweets

---
+++ cmd/poottweet/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type BearerTokenResponse struct {
+	TokenType   string `json:"token_type"`
+	AccessToken string `json:"access_token"`
+}
+
+func main() {
+	// Get configuration.
+	key := flag.String("consumer-key", "", "Twitter API consumer key")
+	secret := flag.String("consumer-secret", "", "Twitter API consumer secret")
+	flag.Parse()
+
+	// Obtain a Bearer token.
+	creds := base64.StdEncoding.EncodeToString([]byte(*key + ":" + *secret))
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		"https://api.twitter.com/oauth2/token",
+		strings.NewReader("grant_type=client_credentials"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	req.Header.Add("Authorization", "Basic "+creds)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var token BearerTokenResponse
+	err = json.Unmarshal(body, &token)
+	if err != nil {
+		panic(err)
+	}
+
+	if token.TokenType != "bearer" {
+		panic("invalid bearer token response: " + string(body))
+	}
+
+	fmt.Printf("%#v\n", token)
+
+	// Drink from the firehose.
+	fhreq, err := http.NewRequest(
+		http.MethodGet,
+		"https://api.twitter.com/labs/1/tweets/stream/sample",
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fhreq.Header.Add("Authorization", "Bearer "+token.AccessToken)
+
+	firehose, err := http.DefaultClient.Do(fhreq)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(firehose.Body)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
